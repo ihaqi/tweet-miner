667ce60ce09f16a6e0d93fcad37e247f7c27fc2e
James Ndegwa Maringa <jmsmaringa@gmail.com>
2013-09-29 03:39:53 +0300

    Update login.py

--- login.py
+++ login.py
@@ -10,11 +10,3 @@ def oauth_login():
     return twitter_api
     
 
-twitter_api=oauth_login()
-
-twitter_stream=twitter.TwitterStream(auth=twitter_api.auth)
-
-q=''  #enter query here
-
-
-stream=twitter_stream.statuses.filter(track=q)
