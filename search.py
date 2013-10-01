d70b1487fa6bc30c44c275dd3e909ad1ced763d3
James Ndegwa Maringa <jmsmaringa@gmail.com>
2013-10-01 15:22:44 +0300

    Update search.py

--- search.py
+++ search.py
@@ -2,3 +2,32 @@ from login import *
 
 twitter_api=oauth_login()
 
+def twitter_search(t, q, max_results=200, **kw):
+
+    
+    search_results = t.search.tweets(q=q, count=100, **kw)
+    
+    statuses = search_results['statuses']
+    
+    max_results = min(1000, max_results)
+    
+    for _ in range(10): # 10*100 = 1000
+        try:
+            next_results = search_results['search_metadata']['next_results']
+        except KeyError, e: # No more results when next_results doesn't exist
+            break
+            
+        # Create a dictionary from next_results, which has the following form:
+        # ?max_id=313519052523986943&q=NCAA&include_entities=1
+        kwargs = dict([ kv.split('=') 
+                        for kv in next_results[1:].split("&") ])
+        
+        search_results = t.search.tweets(**kwargs)
+        statuses += search_results['statuses']
+        
+        if len(statuses) > max_results: 
+            break
+            
+    return statuses
+    
+q='' #enter query here
