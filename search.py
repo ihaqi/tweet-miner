74abbbd49f0bc6da4241b1499061ae8f12b68d46
James Ndegwa Maringa <jmsmaringa@gmail.com>
2013-10-13 16:45:26 +0300

    Update search.py

--- search.py
+++ search.py
@@ -28,6 +28,16 @@ def search(t, query, max_results=200, **kw):
         
         if len(statuses) > max_results: 
             break
+        status_texts = [ status['text'] for status in statuses ]
+
+        screen_names = [ user_mention['screen_name'] 
+                        for status in statuses
+                            for user_mention in status['entities']['user_mentions'] ]
+
+        hashtags = [ hashtag['text'] 
+                    for status in statuses
+                        for hashtag in status['entities']['hashtags'] ]
+        
             
     return statuses
     
